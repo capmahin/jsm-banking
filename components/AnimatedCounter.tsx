05cf42529235223d73aa347cbb1713e1f5fe882a
capmahin <mahinulhuque1996@gmail.com>
2024-05-12 21:20:42 +0600

    add style in div w-full

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,8 +4,10 @@ import CountUp from 'react-countup'
 
 const AnimatedCounter = ({amount}: {amount:number}) => {
   return (
-    <div>
+    <div className='w-full'>
       <CountUp 
+     
+      decimals={2}
       decimal=','
       prefix='$'
       end={amount} />
