0dfb4f03686747da86715c73a315cfca422c2f5e
capmahin <mahinulhuque1996@gmail.com>
2024-05-12 11:30:03 +0600

    add total balance lebal

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -11,8 +11,13 @@ const TotalBalanceBox = ({
 
          <div className='flex flex-col gap-6'>
              <h2 className='header-2'>
-                 {totalBanks}
+             Bank Accounts: {totalBanks}
              </h2>
+             <div className='flex flex-col gap-2'>
+               <p className='total-balance-label'>
+                Total Current Balance
+               </p>
+             </div>
          </div>
     </section>
   )
