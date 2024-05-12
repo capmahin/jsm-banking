d7f87f4ffac1a13e7dfdc2b51ea63985195ce44e
capmahin <mahinulhuque1996@gmail.com>
2024-05-12 11:09:23 +0600

    add p tag and style it

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -8,9 +8,11 @@ user
       <h1 className="header-box-title">
         {title}
         {type === 'greeting' && (
-            <span>{user}</span>
+            <span className="text-bankGradient">
+                &nbsp;{user}</span>
         )}
         </h1>
+        <p className="header-box-subtext">{subtext}</p>
     </div>
   )
 }
