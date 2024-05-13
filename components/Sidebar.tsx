61309e2dc76df1396165485137d1c0c92eade441
capmahin <mahinulhuque1996@gmail.com>
2024-05-13 13:26:03 +0600

    working

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -40,6 +40,11 @@ const Sidebar = ({user}: SiderbarProps) => {
                       })}
                       />
                   </div>
+                  <p className={cn('sidebar-label',{
+                    '!text-white':isActive
+                  })}>
+                    {item.label}
+                  </p>
                 </Link>
             )
          })}
