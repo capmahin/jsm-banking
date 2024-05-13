032c2f0b44b1b56b4bc3546adddb8d31d31495a0
capmahin <mahinulhuque1996@gmail.com>
2024-05-14 02:40:25 +0600

    add image and classname to style

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -6,13 +6,22 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from "next/image"
 
 
 const MobileNav = ({user}: MobileNavProps) => {
   return (
-    <section>
+    <section className="w-full max-w-[264px]">
       <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
+  <SheetTrigger>
+    <Image 
+    src="/icons/hambuger.svg"
+    width={30}
+    height={30}
+    alt="menu"
+    className="cursor-pointer"
+    />
+  </SheetTrigger>
   <SheetContent>
     <SheetHeader>
       <SheetTitle>Are you absolutely sure?</SheetTitle>
