8a992641321abccf9adaa59066efed6b1b944ef9
capmahin <mahinulhuque1996@gmail.com>
2024-05-14 14:27:01 +0600

    add name

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -6,7 +6,11 @@ const BankCard = ({account, userName, showBalance= true}:CreditCardProps) => {
     <div className='flex flex-col'>
         <Link href="/" className='bank-card'>
           <div className='bank-card_content'>
-
+            <div>
+                <h1 className='text-16 font-semibold text-white'>
+                  {account.name || 'Adrian'}
+                </h1>
+            </div>
           </div>
         </Link>
     </div>
