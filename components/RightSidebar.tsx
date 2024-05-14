6e6cfee7dac86221e9e2e4d9557ae4a5a8af3a25
capmahin <mahinulhuque1996@gmail.com>
2024-05-14 11:43:01 +0600

    add style

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,7 +4,10 @@ const RightSidebar = ({user,transactions,banks}: RightSidebarProps) => {
   return (
    <aside className='right-sidebar'>
        <section className='flex flex-col pb-8'>
-           
+           <div className='profile-banner'/>
+           <div className='profile'>
+
+           </div>
        </section>
    </aside>
   )
