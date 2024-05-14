43b1a3ebdce2620d03eccf0a525f8ea77c8a42a4
capmahin <mahinulhuque1996@gmail.com>
2024-05-14 11:57:56 +0600

    add style in span tag

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -6,7 +6,11 @@ const RightSidebar = ({user,transactions,banks}: RightSidebarProps) => {
        <section className='flex flex-col pb-8'>
            <div className='profile-banner'/>
            <div className='profile'>
-
+              <div className='profile-img'>
+                    <span className='text-5xl font-bold text-blue-500'>
+                        {user.firstName[0]}
+                    </span>
+              </div>
            </div>
        </section>
    </aside>
