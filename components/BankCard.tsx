3a60d068f7eb9c479ccc4b545c589c74df370fe9
capmahin <mahinulhuque1996@gmail.com>
2024-05-14 14:21:50 +0600

    add card style

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const BankCard = ({account, userName, showBalance= true}:CreditCardProps) => {
   return (
-    <div>BankCard</div>
+    <div className='flex flex-col'>
+        <Link href="/" className='bank-card'>
+        
+        </Link>
+    </div>
   )
 }
 
