ce2ab3401fd8bc025779f154306dbef630759e4e
capmahin <mahinulhuque1996@gmail.com>
2024-05-16 00:55:02 +0600

    add one

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,9 +18,7 @@ import {
 import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
+    email: z.string().email(),
   })
 
 const AuthForm = ({type}:{type: string}) => {
@@ -28,7 +26,7 @@ const AuthForm = ({type}:{type: string}) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          username: "",
+          email: "",
         },
       })
      
@@ -73,18 +71,23 @@ const AuthForm = ({type}:{type: string}) => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
+            <div className='form-item'>
+              <FormLabel className='form-label'>
+                Email
+              </FormLabel>
+              <div className='flex w-full flex-col'>
+                 <FormControl>
+                  <Input
+                  placeholder='Enter your email'
+                  className='input-class'
+                  {...field}
+                  />
+                 </FormControl>
+                 <FormMessage className='form-message'/>
+              </div>
+            </div>
           )}
         />
         <Button type="submit">Submit</Button>
