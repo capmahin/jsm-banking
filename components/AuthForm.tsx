61cd34fb608019b81c00019ffa359888d4dde84a
capmahin <mahinulhuque1996@gmail.com>
2024-05-15 11:40:38 +0600

    add another div withclass name

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,7 +6,7 @@ const AuthForm = ({type}:{type: string}) => {
   return (
     <section className='auth-form'>
         <header className='flex flex-col gap-5 md:gap-8'>
-        <Link href="/" className=' cursor-pointer flex items-center gap-1 px-4'>
+        <Link href="/" className=' cursor-pointer flex items-center gap-1'>
           <Image
             src="/icons/logo.svg"
             width={34}
@@ -16,6 +16,10 @@ const AuthForm = ({type}:{type: string}) => {
           />
           <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
          </Link> 
+
+         <div className='flex flex-col gap-1 md:gap-3'>
+
+         </div>
         </header>
     </section>
   )
