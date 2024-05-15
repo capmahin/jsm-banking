4e6b2787e962f1d7a951c964ee92f2dcfbd85dda
capmahin <mahinulhuque1996@gmail.com>
2024-05-15 11:39:00 +0600

    addd options

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const AuthForm = ({type}:{type: string}) => {
   return (
-    <section className='auth-form'>{type}</section>
+    <section className='auth-form'>
+        <header className='flex flex-col gap-5 md:gap-8'>
+        <Link href="/" className=' cursor-pointer flex items-center gap-1 px-4'>
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt='Horizon logo'
+            
+          />
+          <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
+         </Link> 
+        </header>
+    </section>
   )
 }
 
