9e25a8991deb1001a0d28027414144303746f7a1
capmahin <mahinulhuque1996@gmail.com>
2024-05-15 11:42:11 +0600

    add useState

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const AuthForm = ({type}:{type: string}) => {
+    const [user, setuser] = useState(null)
   return (
     <section className='auth-form'>
         <header className='flex flex-col gap-5 md:gap-8'>
@@ -18,7 +19,9 @@ const AuthForm = ({type}:{type: string}) => {
          </Link> 
 
          <div className='flex flex-col gap-1 md:gap-3'>
-
+            <h1>
+                {user}
+            </h1>
          </div>
         </header>
     </section>
