8c1f7ec7b5d75278a9c90b13bfc2afb95bca0b66
capmahin <mahinulhuque1996@gmail.com>
2024-05-18 01:34:46 +0600

    add adresss1

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -79,9 +79,33 @@ const AuthForm = ({type}:{type: string}) => {
           type === 'sign-up' &&(
             <> 
               <CustomInput control={form.control}
-       name='email' label='Email'
-       placeholder='Enter your email'
-       />
+               name='firstName' label='First Name'
+             placeholder='Enter your first name'
+               />
+              <CustomInput control={form.control}
+               name='lastName' label='Last Name'
+             placeholder='Enter your last name'
+               />
+              <CustomInput control={form.control}
+               name='address1' label='Address'
+             placeholder='Enter your specific address'
+               />
+              <CustomInput control={form.control}
+               name='state' label='State'
+             placeholder='Example: NY'
+               />
+              <CustomInput control={form.control}
+               name='postalCode' label='Postal Code'
+             placeholder='Example: 11101'
+               />
+              <CustomInput control={form.control}
+               name='dateOfBirth' label='Date of Birth'
+             placeholder='YYYY-MM-DD'
+               />
+              <CustomInput control={form.control}
+               name='ssn' label='SSN'
+             placeholder='Example:1234'
+               />
             
             </>
           )
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,7 +195,17 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 export const authFormSchema = z.object({
+  // sign up
+  firstName: z.string().min(3),
+  lastName:z.string().min(3),
+  address1:  z.string().max(50),
+  city: z.string().max(50),
+  state: z.string().min(2).max(2),
+  postalCode:  z.string().min(3).max(6),
+  dateOfBirth:  z.string().min(3),
+  ssn:  z.string().min(3),
+  // both
   email: z.string().email(),
-  password:z.string().min(8),
+  password: z.string().min(8),
   
 })
