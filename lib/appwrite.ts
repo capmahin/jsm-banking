7990c0935716920056940a284bf0f79045e37b82
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 03:02:42 +0600

    get users

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Client, Account, Databases } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
@@ -34,6 +34,9 @@ export async function createAdminClient() {
     },
     get database(){
         return new Databases(client)
+    },
+    get user(){
+        return new Users(client)
     }
   };
 }
