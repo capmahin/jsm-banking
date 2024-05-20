a404dfbeac2523b9fd0d729d4dfe9ccd7b834833
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 02:15:45 +0600

    add another function for signUp

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,15 @@
 
 export const signIn =async ()=>{
     try {
-        
+        //Mutation / Database /Make fetch
+    } catch (error) {
+        console.error('Error',error);
+    }
+}
+
+export const signUp =async ()=>{
+    try {
+        //Mutation / Database /Make fetch
     } catch (error) {
         console.error('Error',error);
     }
