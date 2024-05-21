2bec11b0d37a6b034a8637843c48d0a3fe33e0b8
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 16:25:52 +0600

    working on login design

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -8,6 +9,14 @@ export default function RootLayout({
       <main className="flex min-h-screen w-full justify-between font-inter">
           
           {children}
+          <div className="auth-asset">
+             <div>
+              <Image
+              src="/icons//auth-image.svg"
+              alt="Auth image"
+              />
+             </div>
+          </div>
       </main>
     );
   }
