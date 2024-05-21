553789f1b1cf5d70fe17816889a7df86e1a3855b
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 15:38:41 +0600

    logout create

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,14 @@
+import { logoutAccount } from '@/lib/actions/user.actions'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const Footer = ({user,type='desktop'}:FooterProps) => {
+    const router = useRouter();
     const handleLogOut = async ()=>{
-        
+        const loggedOut = await logoutAccount();
+
+    if(loggedOut) router.push('/sign-in')
     }
   return (
     <footer className='footer'>
