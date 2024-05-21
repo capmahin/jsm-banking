14f43ef36d5c2866ea0f4861971bb43db1726546
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 14:38:57 +0600

    user add adata

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,15 @@ const Footer = ({user,type='desktop'}:FooterProps) => {
        {type === 'mobile'? 
        'footer_name-mobile': 'footer_name'}>
          <p className='text-xl font-bold text-gray-700'>
-            {user.firstName[0]}
+            {user.name}
          </p>
        </div>
 
        <div className=
        {type === 'mobile'? 
        'footer_name-email': 'footer_email'}>
-          <h1 className='text-14'>
+          <h1 className='text-14 truncate font-normal
+          text-gray-600'>
             {user.name}
           </h1>
        </div>
