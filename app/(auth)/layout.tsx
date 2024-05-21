8ae5bb29094d63128f2a8f2f5514ff48c800e2d6
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 16:26:22 +0600

    add width and heioght

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({
               <Image
               src="/icons//auth-image.svg"
               alt="Auth image"
+              width={500}
+              height={500}
               />
              </div>
           </div>
