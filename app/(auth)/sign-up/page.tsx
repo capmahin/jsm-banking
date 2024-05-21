92f941fa2b5acd3804fedac84edb676c067634d4
capmahin <mahinulhuque1996@gmail.com>
2024-05-21 12:10:00 +0600

    parsestrigyfy

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,6 +4,8 @@ import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const SignUp = async() => {
   const loggedInUser = await getLoggedInUser();
+
+  console.log(loggedInUser)
   return (
     <section className='flex-center size-full max-sm:px-6'>
        <AuthForm type="sign-up"/>
