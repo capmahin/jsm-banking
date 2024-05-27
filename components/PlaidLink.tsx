a803b2ad69c3bd3bc63430247b144ae7937bacb2
capmahin <mahinulhuque1996@gmail.com>
2024-05-28 00:13:03 +0600

    add button with tarnary oparetor

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
   return (
-    <div>PlaidLink</div>
+    <>
+     {variant === 'primary'? (
+        <Button>
+            Connect bank
+        </Button>
+     ): variant === 'ghost'?(
+        <Button>
+            Connect bank
+        </Button>
+     ):(
+        <Button>
+            
+        </Button>
+     )}    
+    </>
   )
 }
 
